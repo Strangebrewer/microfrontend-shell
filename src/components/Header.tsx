00f8ebd1eda6f37c3a6399b84e0fb5ec967e7aee
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2025-12-28 14:51:53 -0700

    more stuff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: FC = () => {
   return (
     <nav className='tw:border tw:border-[#ccc] tw:h-[64px]'>
       <h1>Microfrontend Shell</h1>
-      <button onClick={() => navigate('/')} style={{ marginRight: '1rem' }}>App 1</button>
+      <button className='tw:mr-[1rem]' onClick={() => navigate('/')}>App 1</button>
       <button onClick={() => navigate('/stuff')}>App 2</button>
     </nav>
   );
