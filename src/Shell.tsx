0bd17fc072a293064087c22288c9a04f331de7d8
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-02-23 01:06:27 -0700

    minor fix

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -1,25 +1,7 @@
-// const RO = window.ResizeObserver;
-// if (RO && !(window as any).__RO_PATCHED__) {
-//   (window as any).__RO_PATCHED__ = true;
-
-//   window.ResizeObserver = class extends RO {
-//     constructor(cb: ResizeObserverCallback) {
-//       const createdAt = new Error("[RO] created here").stack;
-
-//       super((entries, observer) => {
-//         console.log(createdAt); // <-- points to the library/component that constructed it
-//         console.log("[RO] targets:", entries.map(e => e.target));
-//         cb(entries, observer);
-//       });
-//     }
-//   };
-// }
-
 import { FC } from 'react';
 import { BaseRouter } from './BaseRouter';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import { createAuthClient } from '@bka-stuff/mfe-utils';
 
 const Shell: FC = () => {
   return (
