f04a4dc928bc472be63f8e707c12f196b8d5a715
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-23 01:09:03 -0600

    update webpack to use mfe-utils

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,54 +1,21 @@
 import type { Configuration } from 'webpack';
-// import { container } from 'webpack';
 import webpack from 'webpack';
 import 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import { createWebpackConfig, defaultShared } from '@bka-stuff/mfe-utils';
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const config: Configuration = {
-  mode: 'development',
-  entry: './src/index.tsx',
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: path.resolve(__dirname, "tsconfig.json"),
-          },
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      }
-    ],
-  },
-
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    symlinks: true,
-    alias: {
-      axios: path.resolve(__dirname, "node_modules/axios"),
-      react: path.resolve(__dirname, "node_modules/react"),
-      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-    },
-  },
-
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    clean: true,
-    uniqueName: 'shell',
-  },
+const config = {
+  ...createWebpackConfig({
+    appName: 'shell',
+    resolve: path.resolve,
+    _dirname: __dirname,
+    port: 3000,
+  }),
 
   plugins: [
     new HtmlWebpackPlugin({
@@ -63,25 +30,9 @@ const config: Configuration = {
         'mfe-job-search': 'mfe_job_search@http://localhost:3002/remoteEntry.js',
       },
 
-      shared: {
-        react: { singleton: true },
-        'react-dom': { singleton: true },
-        'react-router-dom': { singleton: true },
-        "@tanstack/react-query": { singleton: true },
-        zustand: { singleton: true },
-        '@bka-stuff/mfe-utils': { singleton: true },
-      },
+      shared: defaultShared,
     }),
-  ],
-
-  devServer: {
-    port: 3000,
-    hot: false,
-    historyApiFallback: true,
-    devMiddleware: {
-      publicPath: '/',
-    },
-  },
-};
+  ]
+} as Configuration;
 
 export default config;
